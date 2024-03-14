c7e29c19441d71015ec8e69b63a9a2766f5cdd54
anithaa553 <163411915+anithaa553@users.noreply.github.com>
2024-03-14 19:14:00 +0530

    Update index.js

--- src/assets/images/index.js
+++ src/assets/images/index.js
@@ -3,7 +3,7 @@ import logoLight from "./logoLight.png";
 import bannerImgOne from "./banner/bannerImgOne.webp";
 import bannerImgTwo from "./banner/bannerImgTwo.webp";
 import bannerImgThree from "./banner/bannerImgThree.webp";
-import saleImgOne from "./sale/saleImgOne.webp";
+import saleImgOne from "./sale/saleImgOne.webp"; 
 import saleImgTwo from "./sale/saleImgTwo.webp";
 import saleImgThree from "./sale/saleImgThree.webp";
 // ============== Products Start here ====================
