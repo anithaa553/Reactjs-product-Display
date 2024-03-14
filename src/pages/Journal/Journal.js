3c9ff2ae98acf43b5498021e68b4092be5a31db6
anithaa553 <163411915+anithaa553@users.noreply.github.com>
2024-03-14 19:04:48 +0530

    Update Journal.js

--- src/pages/Journal/Journal.js
+++ src/pages/Journal/Journal.js
@@ -3,7 +3,7 @@ import { Link, useLocation } from "react-router-dom";
 import Breadcrumbs from "../../components/pageProps/Breadcrumbs";
 
 const Journal = () => {
-  const location = useLocation();
+  const location = useLocation(); 
   const [prevLocation, setPrevLocation] = useState("");
   useEffect(() => {
     setPrevLocation(location.state.data);
