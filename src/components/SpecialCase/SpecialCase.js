27ca8c30ffe387804ccd124582750cad4a34761d
anithaa553 <163411915+anithaa553@users.noreply.github.com>
2024-03-14 19:08:36 +0530

    Update SpecialCase.js

--- src/components/SpecialCase/SpecialCase.js
+++ src/components/SpecialCase/SpecialCase.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { RiShoppingCart2Fill } from "react-icons/ri";
-import { MdSwitchAccount } from "react-icons/md";
+import { MdSwitchAccount } from "react-icons/md"; 
 import { useSelector } from "react-redux";
 
 const SpecialCase = () => {
