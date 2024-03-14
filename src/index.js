86ae91192f40125dfa10865aebf9584505949370
anithaa553 <163411915+anithaa553@users.noreply.github.com>
2024-03-14 19:00:37 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import { PersistGate } from "redux-persist/integration/react";
 import "slick-carousel/slick/slick.css";
 import { Provider } from "react-redux";
 import { store, persistor } from "./redux/store";
-import "./index.css";
+import "./index.css"; 
 import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
