33c09406850ca655e159e0cf3e52b4454069befc
anithaa553 <163411915+anithaa553@users.noreply.github.com>
2024-03-14 19:03:46 +0530

    Update Payment.js

--- src/pages/payment/Payment.js
+++ src/pages/payment/Payment.js
@@ -6,7 +6,7 @@ const Payment = () => {
   return (
     <div className="max-w-container mx-auto px-4">
       <Breadcrumbs title="Payment gateway" />
-      <div className="pb-10">
+      <div className="pb-10"> 
         <p>Payment gateway only applicable for Production build.</p>
         <Link to="/">
           <button className="w-52 h-10 bg-primeColor text-white text-lg mt-4 hover:bg-black duration-300">
